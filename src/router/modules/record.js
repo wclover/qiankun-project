9521f32bb97ab80a485d6d10773248536356c99f
liangJiaHao <liangjiahao@ftl-express.cn>
2022-03-06 13:23:44 +0800

    fix: fix qiankun router??

--- src/router/modules/record.js
+++ src/router/modules/record.js
@@ -1,17 +1,21 @@
-import Layout from '@/layout'
+// import Layout from '@/layout'
 
 export default {
-  path: '/qiankun',
-  name: 'QianKun',
-  component: Layout,
-  redirect: '/qiankun/record',
-  meta: {title: '乾坤', icon: 'el-icon-menu'},
-  children: [
-    {
-      path: 'record',
-      name: 'QiankunRecord',
-      component: () => import('@/views/RecordIndex'),
-      meta: {title: '乾坤里的记录', icon: 'el-icon-location'}
-    }
-  ]
+  // path: '/qiankun',
+  // name: 'QianKun',
+  // component: Layout,
+  // redirect: '/qiankun/record',
+  // meta: {title: '乾坤', icon: 'el-icon-menu'},
+  // children: [
+  //   {
+  //     path: 'record',
+  //     name: 'QiankunRecord',
+  //     component: () => import('@/views/RecordIndex'),
+  //     meta: {title: '乾坤里的记录', icon: 'el-icon-location'}
+  //   }
+  // ]
+  path: '/qiankun/record',
+  name: 'QiankunRecord',
+  component: () => import('@/views/RecordIndex'),
+  meta: {title: '乾坤里的记录', icon: 'el-icon-location'}
 }
